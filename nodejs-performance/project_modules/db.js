6e31b3517e2a3bf27b9eed4aa6c0ef47e7c5a618
gpiazzolla <gaetano.piazzolla@dxc.com>
2021-02-14 22:56:38 +0100

    db service starts with delay!?

--- nodejs-performance/project_modules/db.js
+++ nodejs-performance/project_modules/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'db-service',
     database: 'postgres',
     password: 'postgres',
     port: 5432,
